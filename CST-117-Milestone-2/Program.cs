778a94ef2cdafcde517c476bf623cbd2eab51510
lindsay <lindsayblood@gmail.com>
2020-11-06 16:35:33 -0600

    Add project files.

---
+++ CST-117-Milestone-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST_117_Milestone_2
+{
+    class Inventory
+    {
+
+        static void Main(string[] args)
+        {
+            InventoryItem item = new InventoryItem("Chips", "A Bag of chips", 10, 2.99M, 0.5M);
+            Console.WriteLine("An item has been created.");
+            Console.WriteLine("Item Name: " + item.Name);
+            Console.WriteLine("Description: " + item.Description);
+            Console.WriteLine("Quantity: " + item.Quantity);
+            Console.WriteLine("Price: " + item.Price);
+            Console.WriteLine("Weight: " + item.Weight);
+
+            Console.ReadKey();
+        }
+    }
+}
